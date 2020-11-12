e5cb1a7ec9463474d339559059d57cd807f54c4a
Maria <zweitesherz@mail.ru>
2020-11-12 18:34:05 +0500

    cleaning code

--- js/js.js
+++ js/js.js
@@ -1,10 +1,11 @@
+
 //переключаю класс при клике на заголовок списка
 $(".panel-heading").on("click", function () {
     $(this).toggleClass('active');
 });
 
 
-
+// Валидация
 // Запрет на введение букв в поле телефоного номера
 document.getElementById('checkNumber').onkeydown = function (e) {
     return !(/^[А-Яа-яA-Za-z ]$/.test(e.key));  // IE > 9
